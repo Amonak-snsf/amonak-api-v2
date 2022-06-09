ff010f538a4b9e72c5ceb9abf4e0d88e5d7a814b
AIKPE ACHILE <aikpeachille55@gmail.com>
2022-06-09 17:28:59 +0000

    merge data

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,7 +2,7 @@ export default () => ({
   db: {
     type: process.env.DATABASE_TYPE || "mongodb",
     host: process.env.DATABASE_HOST || "localhost",
-    port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
+    port: parseInt(process.env.DATABASE_PORT, 10) || 27017,
     name: process.env.DATABASE_NAME || "amonak",
     password: process.env.DATABASE_PASSWORD || "",
   },
