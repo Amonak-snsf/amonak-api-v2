4d42e4e0ccc0e56bb8234f8a3c422bf863265a83
Jeanphilippekokora <jeanpkokora@gmail.com>
2022-06-16 16:00:46 +0200

    nothing

--- dist/toptens/dto/update-topten.dto.d.ts
+++ dist/toptens/dto/update-topten.dto.d.ts
@@ -1,7 +1,7 @@
 import { User } from 'src/users/entities/user.entity';
 import { CreateToptenDto } from './create-topten.dto';
 import { Status } from './topten-status-interface';
-declare const UpdateToptenDto_base: import("@nestjs/common").Type<Partial<Omit<CreateToptenDto, "name" | "webSites" | "content" | "user" | "files" | "productNature" | "price" | "company" | "duration">>>;
+declare const UpdateToptenDto_base: import("@nestjs/common").Type<Partial<Omit<CreateToptenDto, "name" | "webSites" | "content" | "price" | "files" | "user" | "productNature" | "company" | "duration">>>;
 export declare class UpdateToptenDto extends UpdateToptenDto_base {
     followers: User[];
     status: Status;
