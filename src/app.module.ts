f79afc84d5a1f7e66b52c6f6bb53ad6a7ac0fbe0
AIKPE ACHILE <aikpeachille55@gmail.com>
2022-07-02 00:27:14 +0000

    database

--- src/app.module.ts
+++ src/app.module.ts
@@ -31,7 +31,7 @@ import { NotificationsModule } from './notifications/notifications.module';
 import { MessagesModule } from './messages/messages.module';
 import { EventEmitterModule } from '@nestjs/event-emitter';
 //    url: "mongodb://root:F8jtnzkF6Xvt@localhost:27017/amonak-nest?compressors=disabled&gssapiServiceName=mongodb"
-
+// ${config.get('db.username')}:${config.get('db.password')}@
 @Module({
   imports: [
     CacheModule.register({ttl: 3600, isGlobal: true}),
@@ -39,7 +39,7 @@ import { EventEmitterModule } from '@nestjs/event-emitter';
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: async (config: ConfigService) => ({
-        uri: `${config.get('db.type')}://${config.get('db.username')}:${config.get('db.password')}@${config.get('db.host')}:${config.get('db.port')}/${config.get('db.name')}?compressors=disabled&gssapiServiceName=mongodb`,
+        uri: `${config.get('db.type')}://${config.get('db.host')}:${config.get('db.port')}/${config.get('db.name')}`,
       }),
       inject: [ConfigService],
     }),
