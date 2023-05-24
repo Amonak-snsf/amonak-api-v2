71ad18623215a828a33277136f0e9c6505dc5b64
AIKPE <aikpeachille55@gmail.com>
2023-05-24 11:33:07 +0200

    add file to confirm email domaine

--- src/config/database.ts
+++ src/config/database.ts
@@ -6,7 +6,7 @@ export default () => ({
     name: process.env.DATABASE_NAME || "amonak",
     password: process.env.DATABASE_PASSWORD || "F8jtnzkF6Xvt",
     username: process.env.DATABASE_USERNAME || "root",
-    url: 'mongodb://localhost:27017/amonak3-nest',
+    url: 'mongodb://localhost:27017/amonak3-nest'
     //url: "mongodb+srv://root:root2022@cluster0.2eimy.mongodb.net/?retryWrites=true&w=majority",
     //url: "mongodb://user_amonak:Amonak2022$$@51.75.126.93:27017/amonak1?retryWrites=true&w=majority"  
   },
