1bce3ed8e61a8ac32930b74f1605ee45263c17dd
AIKPE ACHILE <58424651+AchoBestman@users.noreply.github.com>
2024-03-16 17:38:10 +0000

    Update database.ts

--- src/config/database.ts
+++ src/config/database.ts
@@ -6,8 +6,9 @@ export default () => ({
     name: process.env.DATABASE_NAME || "amonak",
     password: process.env.DATABASE_PASSWORD || "F8jtnzkF6Xvt",
     username: process.env.DATABASE_USERNAME || "root",
-    url: 'mongodb://localhost:27017/amonak3-nest'
+    //url: 'mongodb://localhost:27017/amonak3-nest'
     //url: "mongodb+srv://root:root2022@cluster0.2eimy.mongodb.net/?retryWrites=true&w=majority",
-    //url: "mongodb://user_amonak:Amonak2022$$@51.75.126.93:27017/amonak1?retryWrites=true&w=majority"  
+    //url: "mongodb://user_amonak:Amonak2022$$@51.75.126.93:27017/amonak1?retryWrites=true&w=majority",
+    url: "mongodb+srv://amonak:P8uQfRzgziS5LBiU@amonak.pxlxz7a.mongodb.net/amonak1?retryWrites=true&w=majority&appName=Amonak",
   },
 });
