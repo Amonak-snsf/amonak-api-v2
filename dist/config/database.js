153c43c8ce858771e67a12404675c534448a8113
AIKPE ACHILE <aikpeachille55@gmail.com>
2024-08-17 11:17:31 +0000

    update

--- dist/config/database.js
+++ dist/config/database.js
@@ -8,7 +8,7 @@ exports.default = () => ({
         name: process.env.DATABASE_NAME || "amonak",
         password: process.env.DATABASE_PASSWORD || "F8jtnzkF6Xvt",
         username: process.env.DATABASE_USERNAME || "root",
-        url: 'mongodb://localhost:27017/amonak3-nest',
+        url: "mongodb+srv://root:TWb85O6kqXsO0227@amonak.wfdx4wo.mongodb.net/amonak?retryWrites=true&w=majority&appName=amonak",
     },
 });
 //# sourceMappingURL=database.js.map
